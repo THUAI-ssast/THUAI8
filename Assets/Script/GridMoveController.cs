dc9035f597345e1367e0ae6f205bdf584ef0254e
Subway <zimuzujun@163.com>
2024-10-08 22:24:24 +0800

    Improve movement,adding path and block judgement.

--- Assets/Script/GridMoveController.cs
+++ Assets/Script/GridMoveController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -11,19 +12,76 @@ public class GridMoveController : MonoBehaviour
     [SerializeField] private Tilemap _wallTilemap;
     [SerializeField] private Tilemap _stuffTilmap;
 
+    [SerializeField] private float _moveCD;
+    private bool _isMovable = true;
+
+    private void Start()
+    {
+        var position = _playerMove.transform.position;
+        _playerMove.SetPosition(position, _grounTilemap.WorldToCell(position));
+    }
+
     private void Update()
     {
+        if (_isMovable)
+            tryMove();
+    }
+
+    private async void tryMove()
+    {
+        Vector3Int cellPos = Vector3Int.back;
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int cellPos = _grounTilemap.WorldToCell(mousePos);
-            if (_grounTilemap.HasTile(cellPos))
-            {
-                if (!_wallTilemap.HasTile(cellPos))
-                {
-                    _playerMove.SetPosition(_grounTilemap.CellToWorld(cellPos)+_grounTilemap.cellSize*0.5f, cellPos);
-                }
-            }
+            Vector3Int targetCellPos = _grounTilemap.WorldToCell(mousePos);
+            if (Math.Abs(targetCellPos.x - _playerMove.TilePosition.x) <= 3 &&
+                Math.Abs(targetCellPos.y - _playerMove.TilePosition.y) <= 3)
+                cellPos = targetCellPos;
         }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            cellPos = _playerMove.TilePosition + Vector3Int.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            cellPos = _playerMove.TilePosition + Vector3Int.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            cellPos = _playerMove.TilePosition + Vector3Int.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            cellPos = _playerMove.TilePosition + Vector3Int.down;
+        }
+
+        List<Vector3> path = new List<Vector3>();
+        if (findTilePath(_playerMove.TilePosition, cellPos, path))
+        {
+            Vector3 bias = _grounTilemap.cellSize * 0.5f;
+            for (int i = 0; i < path.Count; i++)
+                path[i] += bias;
+            _playerMove.SetPosition(_grounTilemap.CellToWorld(cellPos) + _grounTilemap.cellSize * 0.5f, cellPos,
+                path.ToArray());
+            _isMovable = false;
+            await Task.Delay((int)(_moveCD * 1000));
+            _isMovable = true;
+        }
+    }
+
+    private bool findTilePath(Vector3Int from, Vector3Int to, List<Vector3> list)
+    {
+        if ((!_grounTilemap.HasTile(from)) || _wallTilemap.HasTile(from))
+            return false;
+        list.Add(from);
+        if (from == to)
+            return true;
+        Vector3Int horizontal = Math.Sign(to.x - from.x) * Vector3Int.right;
+        Vector3Int vertical = Math.Sign(to.y - from.y) * Vector3Int.up;
+        if ((horizontal != Vector3Int.zero && findTilePath(from + horizontal, to, list))
+            || (vertical != Vector3Int.zero && findTilePath(from + vertical, to, list)))
+            return true;
+        list.RemoveAt(list.Count - 1);
+        return false;
     }
 }
--- Assets/Script/Playr/PlayerMove.cs
+++ Assets/Script/Playr/PlayerMove.cs
@@ -4,18 +4,20 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMove : MonoBehaviour
 {
     private LineRenderer _pathLineRenderer;
 
     private bool _isMoving;
-    private Vector3Int _tilePosition;
+    public Vector3Int TilePosition { get; private set; }
 
     private void Start()
     {
         _pathLineRenderer = GetComponent<LineRenderer>();
     }
+
 
     public bool SetPosition(Vector3 worldPosition, Vector3Int tilePosition)
     {
@@ -25,9 +27,26 @@ public class PlayerMove : MonoBehaviour
         var position = transform.position;
         float duration = (worldPosition - position).magnitude * 0.6f;
         drawPathLine(worldPosition,duration);
-        transform.DORotate(new Vector3(0, 0,Vector3.Angle(Vector3.right,worldPosition-position)),0.1f).SetEase(Ease.Linear);
+        Vector3 moveVector = worldPosition - position;
+        float angle = (moveVector.y > 0 ? 1 : -1) * Vector3.Angle(Vector3.right, moveVector);
+        transform.DORotate(new Vector3(0, 0, angle),0.2f).SetEase(Ease.Linear);
         transform.DOMove(worldPosition, duration);
-        _tilePosition = tilePosition;
+        TilePosition = tilePosition;
+        return true;
+    }
+    public bool SetPosition(Vector3 worldPosition, Vector3Int tilePosition, Vector3[] path)
+    {
+        if (_isMoving||path.Length<2)
+            return false;
+        _isMoving = true;
+        var position = transform.position;
+        float duration = (path.Length - 1) * 0.6f;
+        drawPathLine(path, duration);
+        Vector3 moveVector = path[^1] - path[^2];
+        float angle = (moveVector.y > 0 ? 1 : -1) * Vector3.Angle(Vector3.right, moveVector);
+        transform.DORotate(new Vector3(0, 0, angle), duration*0.5f).SetEase(Ease.Linear);
+        transform.DOPath(path, duration);
+        TilePosition = tilePosition;
         return true;
     }
 
@@ -36,6 +55,15 @@ public class PlayerMove : MonoBehaviour
         _pathLineRenderer.SetPositions(new []{transform.position,target});
         _pathLineRenderer.enabled = true;
         await Task.Delay((int)duration*1000);
+        _pathLineRenderer.enabled = false;
+        _isMoving = false;
+    }
+    private async void drawPathLine(Vector3[] path, float duration)
+    {
+        _pathLineRenderer.positionCount = path.Length;
+        _pathLineRenderer.SetPositions(path);
+        _pathLineRenderer.enabled = true;
+        await Task.Delay((int)duration * 1000);
         _pathLineRenderer.enabled = false;
         _isMoving = false;
     }
