0655091d6d47e069907e741595fecfadf0af0ad9
Ukawka <yx196303@gmail.com>
2024-10-10 22:41:00 +0800

    add startscene

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Mirror;
+using TMPro;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private  GameObject loadingPanel;
+    [SerializeField] private  TextMeshProUGUI matchingText;
+    private int playercount = 1;
+    public void Start()
+    {
+        loadingPanel.SetActive(false);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CancelMatching()
+    {
+        loadingPanel.SetActive(false);
+    }
+    public void StartGame()
+    {   
+        loadingPanel.SetActive(true);
+        // NetworkManager.singleton.StartClient();
+        StartCoroutine(StartMatching());
+    }
+    IEnumerator StartMatching()
+    {
+        while (playercount < 6)
+        {
+            playercount = 1;
+            matchingText.text = "(" + playercount.ToString() + "/6)";
+            yield return new WaitForSeconds(1);
+            playercount = 6;
+        }
+        SceneManager.LoadScene("BattleScene");
+    }
+}
