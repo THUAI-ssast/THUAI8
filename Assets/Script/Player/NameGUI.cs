f57813abbfb68281a88a8052bce972900952d005
Musong <wenghk22@mails.tsinghua.edu.cn>
2024-10-25 22:47:54 +0800

    finish sceneJump and Name/Health UI

---
+++ Assets/Script/Player/NameGUI.cs
@@ -0,0 +1,37 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 用于显示玩家在BattleScene中的名字 
+/// </summary>
+public class NameGUI : NetworkBehaviour
+{
+    /// <summary>
+    /// 对应的玩家Transform
+    /// </summary>
+    public Transform TargetPlayer;
+
+    /// <summary>
+    /// 相对于玩家位置的偏移量，用于调整玩家名字的显示位置
+    /// </summary>
+    public Vector3 Offset;
+
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        gameObject.GetComponent<TMP_Text>().text = TargetPlayer.gameObject.GetComponent<PlayerHealth>().Name;
+        transform.position = TargetPlayer.transform.position + Offset;
+        transform.rotation = Quaternion.identity;
+    }
+}
---
+++ Assets/Script/Player/PlayerHealth.cs
@@ -0,0 +1,142 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+/// <summary>
+/// 该类中保存了玩家的名字、血量上限和当前血量，以及处理血量变化逻辑的方法
+/// </summary>
+public class PlayerHealth : NetworkBehaviour
+{
+    /// <summary>
+    /// 玩家的名字
+    /// </summary>
+    [SyncVar] public string Name;
+    
+    /// <summary>
+    /// 玩家的总血量上限
+    /// </summary>
+    public float TotalMaxHealth { get => HeadMaxHealth + BodyMaxHealth + LegMaxHealth; }
+    /// <summary>
+    /// 玩家的头部血量上限
+    /// </summary>
+    public float HeadMaxHealth;
+    /// <summary>
+    /// 玩家的身体血量上限
+    /// </summary>
+    public float BodyMaxHealth;
+    /// <summary>
+    /// 玩家的腿部血量上限
+    /// </summary>
+    public float LegMaxHealth;
+
+    /// <summary>
+    /// 玩家的当前总血量
+    /// </summary>
+    public float TotalHealth { get => HeadHealth + BodyHealth + LegHealth; }
+    /// <summary>
+    /// 玩家的当前头部血量，变化时会调用HeadHealthChange函数
+    /// </summary>
+    [SyncVar(hook = nameof(HeadHealthChange))] public float HeadHealth;
+    /// <summary>
+    /// 玩家的当前身体血量，变化时会调用BodyHealthChange函数
+    /// </summary>
+    [SyncVar(hook = nameof(BodyHealthChange))] public float BodyHealth;
+    /// <summary>
+    /// 玩家的当前腿部血量，变化时会调用LegHealthChange函数
+    /// </summary>
+    [SyncVar(hook = nameof(LegHealthChange))] public float LegHealth;
+
+    /// <summary>
+    /// 对应的玩家对象Transform
+    /// </summary>
+    public Transform TargetPlayer;
+
+    /// <summary>
+    /// 玩家的头部血条UI
+    /// </summary>
+    public HealthGUI HeadHealthGUI;
+    /// <summary>
+    /// 玩家的身体血条UI
+    /// </summary>
+    public HealthGUI BodyHealthGUI;
+    /// <summary>
+    /// 玩家的腿部血条UI
+    /// </summary>
+    public HealthGUI LegHealthGUI;
+
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+        // 赋予初始血量
+        HeadHealth = BodyHealth = LegHealth = HeadMaxHealth = BodyMaxHealth = LegMaxHealth = 10;
+        // 获取玩家名字
+        if (isLocalPlayer) 
+        {
+           CmdSetName(PlayerPrefs.GetString("Name"));
+        }
+    }
+
+    int cnt = 0; // 调试用
+    // Update is called once per frame
+    void Update()
+    {
+        if (isServer)
+        {
+            cnt++;
+            if (cnt % 1000 == 0)
+            {
+                HeadHealth--;
+                BodyHealth--;
+                LegHealth--;
+            }
+        }
+    }
+
+    /// <summary>
+    /// hook函数，当HeadHealth改变后自动被调用
+    /// </summary>
+    /// <param name="oldHealth"></param>
+    /// <param name="newHealth"></param>
+    public void HeadHealthChange(float oldHealth, float newHealth)
+    {
+        // 找到对应的血条更新显示
+        HeadHealthGUI.UpdateHealthGUILength(HeadMaxHealth, newHealth);
+    }
+
+    /// <summary>
+    /// hook函数，当BodyHealth改变后自动被调用
+    /// </summary>
+    /// <param name="oldHealth"></param>
+    /// <param name="newHealth"></param>
+    public void BodyHealthChange(float oldHealth, float newHealth)
+    {
+        // 找到对应的血条更新显示
+        BodyHealthGUI.UpdateHealthGUILength(BodyMaxHealth, newHealth);
+    }
+
+    /// <summary>
+    /// hook函数，当LegHealth改变后自动被调用
+    /// </summary>
+    /// <param name="oldHealth"></param>
+    /// <param name="newHealth"></param>
+    public void LegHealthChange(float oldHealth, float newHealth)
+    {
+        // 找到对应的血条更新显示
+        LegHealthGUI.UpdateHealthGUILength(LegMaxHealth, newHealth);
+    } 
+
+    /// <summary>
+    /// Command函数，在客户端被调用，但在服务端执行。
+    /// 向服务端同步玩家的名字
+    /// </summary>
+    /// <param name="name">玩家的名字</param>
+    [Command]
+    private void CmdSetName(string name)
+    {
+        Name = name;
+    }
+
+}
