ab4ea6c45460dc7225b1a292b473830697d09694
Ukawka <yx196303@gmail.com>
2024-11-09 01:28:10 +0800

    fix: resolve the issue of garbled comments, add comments.

--- Assets/Script/UI/PlayerInfoUI.cs
+++ Assets/Script/UI/PlayerInfoUI.cs
@@ -4,39 +4,38 @@ using TMPro;
 using UnityEngine;
 
 /// <summary>
-/// ���½������Ϣ���UI
+/// 左下角玩家信息面板UI
 /// </summary>
 public class PlayerInfoUI : MonoBehaviour
 {
     /// <summary>
-    /// ��Ϣ����е�����ֵ����
+    /// 信息面板中的体力值部分
     /// </summary>
     [SerializeField] private GameObject APPanel;
     /// <summary>
-    /// ��Ϣ����е�ͷ��Ѫ������
+    /// 信息面板中的头部血量部分
     /// </summary>
     [SerializeField] private GameObject HeadHPPanel;
     /// <summary>
-    /// ��Ϣ����е�����Ѫ������
+    /// 信息面板中的身体血量部分
     /// </summary>
     [SerializeField] private GameObject BodyHPPanel;
     /// <summary>
-    /// ��Ϣ����е��Ȳ�Ѫ������
+    /// 信息面板中的腿部血量部分
     /// </summary>
     [SerializeField] private GameObject LegHPPanel;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // ��ʾ�������
+        // 显示玩家名字
         transform.Find("NameText").gameObject.GetComponent<TextMeshProUGUI>().text = PlayerPrefs.GetString("Name");
     }
 
     /// <summary>
-    /// ������Ϣ����е�����ֵ����
+    /// 更新信息面板中的体力值部分
     /// </summary>
-    /// <param name="newActionPoint">���º������ֵ</param>
-    /// <param name="maxActionPoint">����ֵ����</param>
+    /// <param name="newActionPoint">更新后的体力值</param>
+    /// <param name="maxActionPoint">体力值上限</param>
     public void UpdateActionPoint(float newActionPoint, float maxActionPoint)
     {  
         APPanel.transform.Find("Bar").GetComponent<UnityEngine.UI.Image>().fillAmount = newActionPoint/ maxActionPoint;
@@ -45,11 +44,11 @@ public class PlayerInfoUI : MonoBehaviour
     }
 
     /// <summary>
-    /// ������Ϣ����е�Ѫ�����֣�����ͷ����������Ȳ�
+    /// 更新信息面板中的血量部分，包括头部、身体和腿部
     /// </summary>
-    /// <param name="newHealth">���º��Ѫ��</param>
-    /// <param name="maxHealth">Ѫ������</param>
-    /// <param name="bodyPart">BodyPart���͵�ö�ٱ���������ָ�����µ�Ѫ������ͷ����������Ȳ��е���һ��</param>
+    /// <param name="newHealth">更新后的血量</param>
+    /// <param name="maxHealth">血量上限</param>
+    /// <param name="bodyPart">BodyPart类型的枚举变量，用于指定更新的血量属于头部、身体和腿部中的哪一个</param>
     public void UpdateHealthPoint(float newHealth, float maxHealth, BodyPart bodyPart)
     {
         GameObject HPPanel;
@@ -73,7 +72,7 @@ public class PlayerInfoUI : MonoBehaviour
 }
 
 /// <summary>
-/// ���岿λö����
+/// 身体部位枚举类
 /// </summary>
 public enum BodyPart
 {
